3aebe7ca973f75055a40eb2bce876eab50de0c00
Sungjin <oksjjj@gmail.com>
2024-02-25 10:59:13 +0900

    2월 25일

---
+++ apps/app.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from apps.config import config
+
+# SQLAlchemy를 인스턴스화한다
+db = SQLAlchemy()
+
+csrf = CSRFProtect()
+
+# config의 키를 전달한다
+def create_app(config_key):
+    # 플라스크 인스턴스 생성
+    app = Flask(__name__)
+
+    # config_key에 매치되는 환경의 config 클래스를 읽어 들인다
+    app.config.from_object(config[config_key])
+
+    # SQLAlchemy와 앱을 연계한다
+    db.init_app(app)
+
+    csrf.init_app(app)
+
+    # Migrate와 앱을 연계한다
+    Migrate(app, db)
+
+    # crud 패키지로부터 views를 import한다
+    from apps.crud import views as crud_views
+
+    # register_blueprint를 사용해 views의 crud를 앱에 등록한다
+    app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    return app
